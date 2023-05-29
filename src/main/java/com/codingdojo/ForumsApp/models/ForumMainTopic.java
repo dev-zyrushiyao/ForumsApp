a8a6415b87e92c6a2a5c7697e0ce94a7c470c97e
Zyrus Hiyao <zyrushiyao@gmail.com>
2023-05-29 20:28:20 +0800

    added a Main Topics ORM

---
+++ src/main/java/com/codingdojo/ForumsApp/models/ForumMainTopic.java
@@ -0,0 +1,10 @@
+package com.codingdojo.ForumsApp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="main_topic")
+public class ForumMainTopic {
+
+}
