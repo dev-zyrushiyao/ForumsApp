f78522a19fc3def635c592fcaebecfc88f745f11
alex-s4 <al39726867@gmail.com>
2023-06-24 21:26:30 +0800

    Customized non-existing pages, for better handling of Whitelabel page

--- src/main/java/com/codingdojo/ForumsApp/ForumsAppApplication.java
+++ src/main/java/com/codingdojo/ForumsApp/ForumsAppApplication.java
@@ -1,11 +1,9 @@
 package com.codingdojo.ForumsApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+// @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class ForumsAppApplication {
 
